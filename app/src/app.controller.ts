bdb1506ed5619ec6469461e60726559448b75106
Pyunghyuk Yoo <yoophi@gmail.com>
2022-07-27 22:55:14 +0900

    ✨ ADD: ConfigModule

--- app/src/app.controller.ts
+++ app/src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+
+interface DatabaseConfig {
+  host: string;
+  port: number;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('config')
+  getConfig(): string {
+    return this.configService.get('database');
   }
 }
